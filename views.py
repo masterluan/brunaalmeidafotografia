b1fa657def78e23f623e6d5fe99e9074e02f95c9
Luan <luancarlosm.porphirio@hotmail.com.br>
2018-08-19 21:32:05 -0300

    aa

--- views.py
+++ views.py
@@ -7,11 +7,35 @@ def configure(app):
     def pagina_inicial():
         return render_template('index.html')
 
-    @app.route("/portifolio")
-    def portifolio():
+    @app.route("/port_criancas")
+    def port_criancas():
         #conexao com o banco de dados para buscarga o problema
         return render_template(
-            'portifolio.html'#, 
+            'port_criancas.html'#, 
+          #  problema = problema ,
+        
+        )
+   # @app.route("/port_casais")
+   # def port_casais():
+        #conexao com o banco de dados para buscarga o problema
+    #    return render_template(
+     #       'port_casais.html'#, 
+          #  problema = problema ,
+        
+     #   )
+    # @app.route("/port_mulheres")
+    #def port_mulheres():
+        #conexao com o banco de dados para buscarga o problema
+    #    return render_template(
+    #        'port_mulheres.html'#, 
+          #  problema = problema ,
+        
+    #    )
+    @app.route("/contato")
+    def contato():
+        #conexao com o banco de dados para buscarga o problema
+        return render_template(
+            'contato.html'#, 
           #  problema = problema ,
         
         )
